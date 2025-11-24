6036c1d281e0a3a02c6a9b3840c331c0652eb846
bacconi.pietro <bacconi.pietro@gmail.com>
2025-11-24 13:02:58 +0100

    add scripts

---
+++ 99_my_scripts/mapping_contaminants.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Mapping short reads on POLISHED assembly FOR CONTAMINANTS DETECTION. Mutate SAM into BAM
+
+# Short reads
+minimap2 -ax sr --MD -t 8 Anoste_pol.fasta SRR11672503_1_paired.fastq SRR11672503_2_paired.fastq | samtools view -Sb - > Anoste_pol_sr.bam
+samtools sort -@8 -o Anoste_pol_sr_sorted.bam Anoste_pol_sr.bam
+samtools index Anoste_pol_sr_sorted.bam
+rm Anoste_pol_sr.bam
+
+
+
+
+
